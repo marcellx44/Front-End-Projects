799d98246502ea4810732b81957c25d23d657b0d
Marcella Oliveira <marcella.oliveira.diasss@gmail.com>
2025-09-24 14:01:37 -0300

    Função inserir com o Click funcionando

--- To-Do-List/script.js
+++ To-Do-List/script.js
@@ -12,7 +12,12 @@ elemento.textContent="Tarefa0";
 
 /*Inserir o elemento */
 
-lista.insertBefore(elemento,itens[1]);
+//lista.insertBefore(elemento,itens[1]);
 
 /*Adicionar ao clicar */
 var botao= document.getElementById("btn-add");
+botao.addEventListener("click", inserir);
+
+function inserir(){
+    lista.insertBefore(elemento,itens[0]);
+}
