dc93d8b3d59013f211c0733a4dbb6c7234f2c219
Marcella <marcella.oliveira.diasss@gmail.com>
2025-09-23 22:34:24 -0300

    Adicionei o projeto To-Do-List em andamento

---
+++ To-Do-List/script.js
@@ -0,0 +1,15 @@
+/*Acessar onde o elemento será inserido */
+var lista= document.getElementById("lista");
+
+/*Acessar os elementos da lista */
+
+var itens= lista.children;
+
+/*Criar o novo elemento e preencher */
+
+var elemento= document.createElement("li");
+elemento.textContent("Tarefa0");
+
+/*Inserir o elemento */
+
+lista.insertBefore(elemento,itens[1]);
